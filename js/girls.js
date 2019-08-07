b85f1f8f4ba1d2d2b3fc9afe35d34a23aa2ce2d6
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-08-07 11:25:32 +0200

    added code when click on dresses link it shows dresses

--- js/girls.js
+++ js/girls.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-    $("dropdown.dropdown-item").click(function(){
-        $("#dresses").toggle();
+    //click on Dresses&Skirts
+    $(".nav-link#skirts").click(function(){
+        $("#background").hide();
+        $("#dresses").show();
       });
+
+      $(".nav-link#skirts").click(function(){
+        $("#pants").hide();
+        // $("#dresses").show();
+      });
+//click on Pants&Shorts
+      $(".nav-link#shorts").click(function(){
+        $("#background").hide();
+        $("#dresses").hide();
+        $("#pants").show();
+      });
+      //click on T-shirt&Shirt
 })
