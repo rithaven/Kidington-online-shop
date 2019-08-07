edfba20e4bce815eb5153aa140ead5574d06e6c8
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-08-07 12:20:43 +0200

    added code when click on pants link it shows pants and shorts

--- js/girls.js
+++ js/girls.js
@@ -1,19 +1,50 @@
-$(document).ready(function(){
-    //click on Dresses&Skirts
-    $(".nav-link#skirts").click(function(){
+$(document).ready(function () {
+    //click on Dresses&Skirts link
+    $(".nav-link#skirts").click(function () {
         $("#background").hide();
-        $("#dresses").show();
-      });
-
-      $(".nav-link#skirts").click(function(){
         $("#pants").hide();
-        // $("#dresses").show();
-      });
-//click on Pants&Shorts
-      $(".nav-link#shorts").click(function(){
+        $("#shirt").hide();
+        $("#sweats").hide();
+        $("#shoes").hide();
+        $("#dresses").show();
+    });
+
+    //click on Pants&Shorts link
+    $(".nav-link#shorts").click(function () {
         $("#background").hide();
         $("#dresses").hide();
+        $("#shirt").hide();
+        $("#sweats").hide();
+        $("#shoes").hide();
         $("#pants").show();
-      });
-      //click on T-shirt&Shirt
+    });
+
+    //click on T-shirt&Shirt link
+    $(".nav-link#Tshirt").click(function () {
+        $("#background").hide();
+        $("#dresses").hide();
+        $("#sweats").hide();
+        $("#pants").hide();
+        $("#shoes").hide();
+        $("#shirt").show();
+    });
+     //click on Sweats&Jackets link
+     $(".nav-link#jackets").click(function () {
+        $("#background").hide();
+        $("#dresses").hide();
+        $("#shirt").hide();
+        $("#shoes").hide();
+        $("#pants").hide();
+        $("#sweats").show();
+    });
+
+    //click on Shoes link
+    $(".nav-link#inkweto").click(function () {
+        $("#background").hide();
+        $("#dresses").hide();
+        $("#sweats").hide();
+        $("#pants").hide();
+        $("#shirt").hide();
+        $("#shoes").show();
+    });
 })
